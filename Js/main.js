31fd2746e89cff99d03115942a9b9e7378dc26bf
Sap <khoctrongmua1q@gmail.com>
2023-07-11 15:47:49 +0700

    Them js

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,26 @@
-// alert('Hi javascript!')
-var fullName = 'Nguyen The Anh';
-var age = 21;
+
+// 1.alert('Hi javascript!')
+
+// 2.var fullName = 'Nguyen The Anh';
+// var age = 21;
 
 
-alert(fullName);
-alert(age);
+// alert(fullName);
+// alert(age);
+
+/**
+ * 
+ */
+
+// 3. built-int hàm xây dựng sẵn chỉ lấy ra dùng
+/**
+ * Alert
+ * Console 
+ * Confirm
+ * Prompt
+ * Set timeout
+ * Set interval
+ */
+
+
+
