1d8714f42c239dd4302c8ec4d7933a537eec0e50
Sap <khoctrongmua1q@gmail.com>
2023-07-11 16:01:39 +0700

    Alert

--- Js/main.js
+++ Js/main.js
@@ -15,12 +15,26 @@
 // 3. built-int hàm xây dựng sẵn chỉ lấy ra dùng
 /**
  * Alert
- * Console 
+ * Console in ra dùng chữ thông báo ở console
  * Confirm
- * Prompt
- * Set timeout
- * Set interval
+ * Prompt xác nhận có thể nhập input
+ * Set timeout cho 1 đoạn code được chạy sau 1 khoảng thời gian
+ * Set interval giống time out nhưng sau 1 giây nó chạy liên tục
  */
 
+// console.log('Day la 1 dong log')
+
+// confirm('Xác nhận bạn đủ tuổi')
+
+// prompt('Xác nhận bạn đủ tuổi')
+
+// setTimeout(function () {
+//  alert('Thông báo')
+// },1000) 
+// sau 1 giây thì thông báo dc chạy
+
+setInterval(function () {
+    alert('Thông báo'+Math.random)
+},1000)
 
 
